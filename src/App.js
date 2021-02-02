22a8ae79ca9a72e5b4d2dc630a9cafee36b035f5
irina <irisha.karpova@gmail.com>
2021-02-01 18:13:14 -0600

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from './components/barPanel/appBar/appBar'
 import { AppBarStore } from './store/appBarStore';
 import { GetParams } from './utility/useAppParams'
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme'
 
 function App() {
 
   return (
-    <AppBarStore.Provider value={GetParams()}>
-      <CssBaseline />
-      <AppBar />
-    </AppBarStore.Provider>
+    <ThemeProvider  theme={theme}>
+      <AppBarStore.Provider value={GetParams()}>
+        <CssBaseline />
+        <AppBar />
+      </AppBarStore.Provider>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './theme'
 import App from './App';
-import { BrowserRouter as Router, Route} from "react-router-dom"
-
 
 ReactDOM.render(
-    <ThemeProvider  theme={theme}>
-      <Router>
-        <Route path="/" render={()=>{return <App/>}}/>
-      </Router>
-    </ThemeProvider>,
+    <App/>,
   document.getElementById('root')
 );
